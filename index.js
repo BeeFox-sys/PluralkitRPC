330aa0eb2288a3bad4ed26aae88b812a9a243752
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-03-07 13:44:16 +1100

    Inital Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const clientId = '685644933797117999';
+const DiscordRPC = require('discord-rpc');
+const bent = require('bent')
+const getJSON = bent('json',200,404,403)
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+var system = null
+var members = null
+async function updateInfo(){
+    console.debug(`${new Date().toISOString()}: Updating Info`)
+    system = await getJSON(`https://api.pluralkit.me/v1/a/${rpc.user.id}`)
+    members = await getJSON(`https://api.pluralkit.me/v1/s/${system.id}/members`)
+}
+async function setActivity(){
+    console.debug(`${new Date().toISOString()}: Updating Rich Presence`)
+    let switchEntry = await getJSON(`https://api.pluralkit.me/v1/s/${system.id}/fronters`)
+    let fronters = switchEntry.members || null
+    let timestamp = null
+    if(fronters) timestamp = new Date(switchEntry.timestamp)
+
+    let name = trunicate(system.name,113) + ` (${members.length} members)`
+
+    let desc = "If you are seeing this, something went wrong"
+    // if(switchEntry == "Unauthorized to view fronter.") desc = 
+    const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+    if(!fronters){
+        desc = `${members.length} members`
+        name = trunicate(system.name,128)
+    } else if(!fronters.length){
+        desc = `No Fronter`
+    } else {
+        for (let index = 0; index < fronters.length; index++) {
+            const names = fronters.map(m=>m.name)
+            desc = formatter.format(names.slice(0,index+1))
+            if(desc.length > 114){
+                desc = formatter.format(names.slice(0,index))+` and ${fronters.length-(index+1)} more`
+                break
+            }
+        }
+    }
+    rpc.setActivity({
+        details: name,
+        state: desc,
+        largeImageKey: "large",
+        largeImageText: "Pluralkit",
+        partyMax: (fronters && fronters.length)?members.length:null,
+        partySize: (fronters && fronters.length)?fronters.length:null,
+        startTimestamp: (fronters)?timestamp:null,
+        instance: false,
+      })
+}
+
+rpc.on('ready',async () => {  
+    console.debug(`${new Date().toISOString()}: Connected to ${rpc.user.username}#${rpc.user.discriminator} (${rpc.user.id}) via application ${rpc.clientId}`)
+    await updateInfo()
+    setInterval(() => {
+        updateInfo();
+    }, 36e5); //1h
+    setActivity();
+    setInterval(() => {
+        setActivity();
+    }, 15e3); // 15s
+})
+rpc.login({ clientId }).catch(console.log);
+
+
+function trunicate(string,length){
+    if(string.length < length) return string
+    return string.substring(0,length-1)+"…"
+}
